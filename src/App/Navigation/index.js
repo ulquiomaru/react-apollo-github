d3726175961625aa07e886d4f2a6fba00a5fb971
Barış Görgülü <baris.gorgulu@gmail.com>
2020-08-31 17:24:01 +0300

    lift organization state up and implement dynamic graphql query with the search field

--- src/App/Navigation/index.js
+++ src/App/Navigation/index.js
@@ -1,19 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 import * as routes from '../../constants/routes';
 
+import Button from '../../Button';
+import Input from '../../Input';
+
 import './style.css';
 
-const Navigation = () => (
-  <header className="Navigation">
-    <div className="Navigation-link">
-      <Link to={routes.PROFILE}>Profile</Link>
-    </div>
-    <div className="Navigation-link">
-      <Link to={routes.ORGANIZATION}>Organization</Link>
-    </div>
-  </header>
-);
+const Navigation = ({
+  location: { pathname },
+  organizationName,
+  onOrganizationSearch,
+}) => {
+  return (
+    <header className="Navigation">
+      <div className="Navigation-link">
+        <Link to={routes.PROFILE}>Profile</Link>
+      </div>
+      <div className="Navigation-link">
+        <Link to={routes.ORGANIZATION}>Organization</Link>
+      </div>
 
-export default Navigation;
+      {pathname === routes.ORGANIZATION && (
+        <OrganizationSearch
+          organizationName={organizationName}
+          onOrganizationSearch={onOrganizationSearch}
+        />
+      )}
+    </header>
+  );
+};
+
+const OrganizationSearch = ({ organizationName, onOrganizationSearch }) => {
+  const [value, setValue] = useState(organizationName);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    onOrganizationSearch(value);
+
+    event.preventDefault();
+  };
+
+  return (
+    <div className="Navigation-search">
+      <form onSubmit={onSubmit}>
+        <Input color={'white'} type="text" value={value} onChange={onChange} />{' '}
+        <Button color={'white'} type="submit">
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default withRouter(Navigation);
--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from './Navigation';
@@ -9,32 +9,45 @@ import * as routes from '../constants/routes';
 
 import './style.css';
 
-const App = () => (
-  <Router>
-    <div className="App">
-      <Navigation />
-      <div className="App-main">
-        <Route
-          exact
-          path={routes.ORGANIZATION}
-          component={() => (
-            <div className="App-content_large-header">
-              <Organization organizationName={'the-road-to-learn-react'} />
-            </div>
-          )}
+const App = () => {
+  const [organizationName, setOrganizationName] = useState(
+    'the-road-to-learn-react',
+  );
+
+  const onOrganizationSearch = (value) => {
+    setOrganizationName(value);
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <Navigation
+          organizationName={organizationName}
+          onOrganizationSearch={onOrganizationSearch}
         />
-        <Route
-          exact
-          path={routes.PROFILE}
-          component={() => (
-            <div className="App-content_small-header">
-              <Profile />
-            </div>
-          )}
-        />
+        <div className="App-main">
+          <Route
+            exact
+            path={routes.ORGANIZATION}
+            component={() => (
+              <div className="App-content_large-header">
+                <Organization organizationName={organizationName} />
+              </div>
+            )}
+          />
+          <Route
+            exact
+            path={routes.PROFILE}
+            component={() => (
+              <div className="App-content_small-header">
+                <Profile />
+              </div>
+            )}
+          />
+        </div>
       </div>
-    </div>
-  </Router>
-);
+    </Router>
+  );
+};
 
 export default App;
